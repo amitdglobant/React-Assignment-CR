912f0da0578424b6a83ebae412e5a700d0f00403
Shailesh Nagpure <GS-1317@GSLAB.COM>
2019-10-12 15:54:14 +0530

    Added test case

--- src/components/MovieList/MovieList.test.js
+++ src/components/MovieList/MovieList.test.js
@@ -11,7 +11,7 @@ configure({ adapter: new Adapter() });
 describe("MovieList component", () => {
   it("should render correctly when no data received in props", () => {
     const output = shallow(<MovieList />);
-    // TODO:: add assertion for this test case here
+    expect(output.find(".no-data").text()).toBe("No movies found to display");
 
   });
   it("should render correctly when valid data received in props", () => {
